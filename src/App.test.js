4cdd64004069bcf6efefe9fa71d50cf4cd8fe6f1
Mateusz <wirus006@gmail.com>
2020-10-18 08:44:00 +0200

    test configuration

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloElement = getByText(/hello react!/i);
+  expect(helloElement).toBeInTheDocument();
 });
