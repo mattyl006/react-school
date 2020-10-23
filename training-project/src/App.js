9f7610a42fe2ffb38ae4e6f22b66e6fb7b58315d
Mateusz <wirus006@gmail.com>
2020-10-23 10:09:57 +0200

    create first component News

--- training-project/src/App.js
+++ training-project/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 
+function News() {
+    return (
+        <section>
+            <h2>Header</h2>
+            <p>Intro</p>
+        </section>
+    );
+} // Ostatnio taki zapis funkcyjny jest bardziej popularny
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React School!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello React School!</h1>
+            <News h2="Header 1" p="Intro 1" />
+            <News h2="Header 2: Breaking news" p="Intro 2" />
+            <News h2="Header 3" p="Intro 3: Noc mnie woła a ja wole grać w lola" />
+        </div>
+    );
 }
 
 export default App;
