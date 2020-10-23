df2b1215b24df0738bdedaba5f46d7747fb8b4ee
Mateusz <wirus006@gmail.com>
2020-10-23 10:35:09 +0200

    News component: refactor by destructuring props

--- training-project/src/App.js
+++ training-project/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function News() {
+function News({header, intro}) { // dane przekazywane do funkcji w postaci json to tak zwany "props"
     return (
         <section>
-            <h2>Header</h2>
-            <p>Intro</p>
+            <h2>{header}</h2>
+            <p>{intro}</p>
         </section>
     );
-} // Ostatnio taki zapis funkcyjny jest bardziej popularny
+} /* za pomocą klamerek {} bindujemy jakieś dynamiczne wartości, bez nich dostaniemy normalny
+łańcuch string */
 
 function App() {
     return (
         <div>
             <h1>Hello React School!</h1>
-            <News h2="Header 1" p="Intro 1" />
-            <News h2="Header 2: Breaking news" p="Intro 2" />
-            <News h2="Header 3" p="Intro 3: Noc mnie woła a ja wole grać w lola" />
+            <News header="Header 1" intro="Intro 1" />
+            <News header="Header 2: Breaking news" intro="Intro 2" />
+            <News header="Header 3" intro="Intro 3: Noc mnie woła a ja wole grać w lola" />
         </div>
     );
 }
