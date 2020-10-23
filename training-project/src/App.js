33e053edd2d3cf3a9185d948a7571122a91dfd77
Mateusz <wirus006@gmail.com>
2020-10-23 13:26:12 +0200

    if statement in React

--- training-project/src/App.js
+++ training-project/src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function News({header, intro}) { // dane przekazywane do funkcji w postaci json to tak zwany "props"
+const DAY = 'monday';
+
+function News({header, intro}) {
     return (
         <section>
             <h2>{header}</h2>
             <p>{intro}</p>
         </section>
     );
-} /* za pomocą klamerek {} bindujemy jakieś dynamiczne wartości, bez nich dostaniemy normalny
-łańcuch string */
+}
 
 function App() {
+    const checkDay = () => {
+        if(DAY === 'monday') {
+            return (
+                <div>
+                    <h1>Hello React School!</h1>
+                    <News header="Header 1" intro="Intro 1" />
+                    <News header="Header 2: Breaking news" intro="Intro 2" />
+                    <News header="Header 3" intro="Intro 3: Noc mnie woła a ja wole grać w lola" />
+                </div>
+            );
+        }
+        else {
+            return(
+                <div>
+                    <h1>Hello React School!</h1>
+                    <p>We dont have news from monday.</p>
+                </div>
+            );
+        }
+    }
     return (
         <div>
-            <h1>Hello React School!</h1>
-            <News header="Header 1" intro="Intro 1" />
-            <News header="Header 2: Breaking news" intro="Intro 2" />
-            <News header="Header 3" intro="Intro 3: Noc mnie woła a ja wole grać w lola" />
+            {checkDay()}
         </div>
     );
 }
