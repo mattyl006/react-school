f27a7ffdc04a19072af8d4d1542172fdef40b1d4
Mateusz <wirus006@gmail.com>
2020-10-24 13:17:40 +0200

    import and use Fragment component to better DOM structure

--- training-project/src/App.js
+++ training-project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
 
 const DAY = 'monday';
@@ -16,23 +16,23 @@ function App() {
     const checkDay = () => {
         if(DAY === 'monday') {
             return (
-                <div>
+                <Fragment>
                     <h1>Hello React School!</h1>
                     <News header="Header 1" intro="Intro 1" />
                     <News header="Header 2: Breaking news" intro="Intro 2" />
                     <News header="Header 3" intro="Intro 3: Noc mnie woła a ja wole grać w lola" />
-                </div>
+                </Fragment>
             );
         }
         else {
             return(
-                <div>
+                <Fragment>
                     <h1>Hello React School!</h1>
                     <p>We dont have news from monday.</p>
-                </div>
+                </Fragment>
             );
         }
-    }
+    };
     return (
         <div>
             {checkDay()}
