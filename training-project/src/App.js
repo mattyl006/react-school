e64b14b852a314e3dd81ac32e9469e659ac504d5
Mateusz <wirus006@gmail.com>
2020-10-28 18:38:03 +0100

    remove service worker from project

--- training-project/src/App.js
+++ training-project/src/App.js
@@ -2,20 +2,24 @@ import React, {Fragment, useState} from 'react';
 import './App.css';
 
 const newsData = [
-    {id: 1, title: 'Important! Goverment in chaos!', intro: 'Mr President broke down.'},
-    {id: 2, title: 'You can be fit before one month!', intro: 'New diet compile by korean pop stars!'},
-    {id: 3, title: 'Linux overthrow Windows.', intro: 'All developers recognize that Windows is very shoddy and start use only Linux systems.'},
-    {id: 4, title: 'Mac Os become open source!', intro: 'Apple have recognized that Mac Os is the same as Linux softwares so they have shared Mac Os as open source.'},
+    {id: 1, author: 'Marcello', title: 'Important! Goverment in chaos!', intro: 'Mr President broke down.'},
+    {id: 2, author: 'Matchew', title: 'You can be fit before one month!', intro: 'New diet compile by korean pop stars!'},
+    {id: 3, author: 'Martin', title: 'Linux overthrow Windows.', intro: 'All developers recognize that Windows is very shoddy and start use only Linux systems.'},
+    {id: 4, author: 'Davido', title: 'Mac Os become open source!', intro: 'Apple have recognized that Mac Os is the same as Linux softwares so they have shared Mac Os as open source.'},
 ];
 
-function News({ header, intro }) {
+const Scoring = ( {score} ) => <p>Ocena: {score}</p>;
+const Author = ( {author} ) => <p>Author: {author}</p>;
+
+function News({ header, intro, author }) {
     const [score] = useState(5);
 
     return (
         <div style={styles}>
             <h2>{header}</h2>
             <p style={styles.p}>{intro}</p>
-            <p>Ocena: {score}</p>
+            <Author author={author} />
+            <Scoring score={score} />
         </div>
     );
 }
@@ -35,7 +39,12 @@ function App() {
             <h1>Hello React School!</h1>
             <div>
                 {newsData.map((elem) => (
-                    <News key={`news-${elem.id}`} header={elem.title} intro={elem.intro} />
+                    <News
+                        key={`news-${elem.id}`}
+                        header={elem.title}
+                        intro={elem.intro}
+                        author={elem.author}
+                    />
                 ))}
             </div>
         </Fragment>
