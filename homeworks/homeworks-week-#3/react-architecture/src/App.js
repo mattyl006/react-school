56181dfbe89f89644551046e0437f246073aca6a
Mateusz <wirus006@gmail.com>
2020-11-01 20:18:58 +0100

    set default values for props color and bgColor and create Input component

--- homeworks/homeworks-week-#3/react-architecture/src/App.js
+++ homeworks/homeworks-week-#3/react-architecture/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Button from "./components/Button";
+import Input from "./components/Input";
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
-const alizarin = "#e74c3c";
-const turquoise = "#1abc9c";
+const colors = {
+    alizarin: '#e74c3c',
+    turquoise: '#1abc9c',
+    emerald: '#2ecc71',
+};
 
 function App() {
   return (
       <main className="App">
         <h1>Hello react-architecture!</h1>
-          <Button icon={faCoffee} color={alizarin} bgColor={turquoise}>Click me!</Button>
+          <Button icon={faCoffee} color={colors.alizarin} bgColor={colors.turquoise}>Click me!</Button>
+          <Input color={colors.alizarin} bgColor={colors.turquoise} borderColor={colors.emerald} borderRadius={16} borderSize={12}/>
       </main>
   );
 }
--- homeworks/homeworks-week-#3/react-architecture/src/components/Button.js
+++ homeworks/homeworks-week-#3/react-architecture/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Button({children, bgColor, color, icon}) {
+function Button({children, bgColor="red", color="blue", icon}) {
     return(
         <button className="button" style={{color: color, backgroundColor: bgColor}}>
              <FontAwesomeIcon icon={icon}/> {children}
