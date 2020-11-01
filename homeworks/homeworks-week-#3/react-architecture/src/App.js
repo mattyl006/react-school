0c36eb2e8458b16b7717fb77799b5b5805d0e3f0
Mateusz <wirus006@gmail.com>
2020-11-01 20:28:45 +0100

    create Textarea Component

--- homeworks/homeworks-week-#3/react-architecture/src/App.js
+++ homeworks/homeworks-week-#3/react-architecture/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import Button from "./components/Button";
-import Input from "./components/Input";
+import Button from "./form/Button";
+import Input from "./form/Input";
+import Textarea from "./form/Textarea";
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const colors = {
@@ -15,6 +16,7 @@ function App() {
         <h1>Hello react-architecture!</h1>
           <Button icon={faCoffee} color={colors.alizarin} bgColor={colors.turquoise}>Click me!</Button>
           <Input color={colors.alizarin} bgColor={colors.turquoise} borderColor={colors.emerald} borderRadius={16} borderSize={12}/>
+          <Textarea color={colors.alizarin} bgColor={colors.turquoise} borderColor={colors.emerald} borderRadius={16} borderSize={12}/>
       </main>
   );
 }
