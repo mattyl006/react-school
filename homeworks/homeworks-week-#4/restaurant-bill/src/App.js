f9e75303d326c1938f90c133184f2f55b198a63a
Mateusz <wirus006@gmail.com>
2020-11-26 10:36:49 +0100

    start doing exercise 1 from week IV

---
+++ homeworks/homeworks-week-#4/restaurant-bill/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Form from "./components/Form";
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="App__header">Restaurant Bill</h1>
+        <Form/>
+    </div>
+  );
+}
+
+export default App;
