86a72424a0bb7f46de65b364c2c4d6dde678d622
Mateusz <wirus006@gmail.com>
2021-04-20 19:20:28 +0200

    create leap.js script

---
+++ homeworks/homeworks-week-#1/leap.js
@@ -0,0 +1,32 @@
+const readline = require('readline').createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
+    }
+);
+
+function checkLeapYear(year) {
+    if(((year % 4 === 0) || (year % 400 === 0)) && !(year % 100 === 0)) {
+        return true;
+    }
+    return false;
+}
+
+readline.question('Input YEAR: ', myInput => {
+
+    let YEAR = '';
+
+    if(myInput === 'current') {
+        YEAR = new Date().getFullYear();
+    } else {
+        YEAR = Number(myInput);
+    }
+
+    if(checkLeapYear(YEAR)) {
+        console.log('YES, this is leap year');
+    } else {
+        console.log('NO, this is not leap year');
+    }
+
+    readline.close();
+})
