d054d49b6522db8e12242a837c46365e7a319f85
Mateusz <wirus006@gmail.com>
2021-04-26 21:04:04 +0200

    homeworks-week-2: create UUID component

--- homeworks/homeworks-week-2/src/App.js
+++ homeworks/homeworks-week-2/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import posts from './data/posts';
 import BlogTile from './components/BlogTile';
+import UUID from './components/UUID';
 
 const styles = {
     h1: {
@@ -12,7 +13,10 @@ const styles = {
         margin: '0 0 48px 0',
         fontSize: '36px',
         fontFamily: 'Montserrat Alternates, sans-serif',
-    }
+    },
+    section: {
+        marginBottom: '64px',
+    },
 };
 
 function App() {
@@ -21,20 +25,28 @@ function App() {
         <h1 style={styles.h1}>
             React School - Homeworks week II
         </h1>
-        <h2 style={styles.h2}>
-            Clickbait app
-        </h2>
-        {
-            posts.map((elem) => {
-                return (
-                    <BlogTile
-                        key={`posts-${elem.id}`}
-                        header={elem.title}
-                        paragraph={elem.intro}
-                    />
-                );
-            })
-        }
+        <section style={styles.section}>
+            <h2 style={styles.h2}>
+                Clickbait app
+            </h2>
+            {
+                posts.map((elem) => {
+                    return (
+                        <BlogTile
+                            key={`posts-${elem.id}`}
+                            header={elem.title}
+                            paragraph={elem.intro}
+                        />
+                    );
+                })
+            }
+        </section>
+        <section style={styles.section}>
+            <h2 style={styles.h2}>
+                UUID Generator
+            </h2>
+            <UUID />
+        </section>
     </div>
   );
 }
